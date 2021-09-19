3e4839f4ffd1481ee11d7665581769b9d9660185
pebblepark <subling02@gmail.com>
2021-09-19 23:35:07 +0900

    멤버십등록 테스트 추가

---
+++ src/test/java/com/example/membership/entity/MembershipRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.example.membership.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class MembershipRepositoryTest {
+
+    @Autowired
+    private MembershipRepository membershipRepository;
+
+    @Test
+    public void 멤버십등록() {
+        //given
+        final Membership membership = Membership.Builder()
+                .userId("userId")
+                .membershipName("네이버")
+                .point(1000)
+                .build();
+
+        //when
+        final Membership result = membershipRepository.save(membership);
+
+        //then
+        assertNotNull(result.getId());
+        assertEquals("userId", result.getUserId());
+        assertEquals("네이버", result.getMembershipName());
+        assertEquals(1000, result.getPoint());
+
+    }
+}
