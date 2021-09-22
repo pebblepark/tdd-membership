b1b2a50f470de36a5360d88f6de8bad409c73406
pebblepark <subling02@gmail.com>
2021-09-22 22:39:18 +0900

    ResponseEntityExceptionHandler 구현

--- src/main/java/com/example/membership/controller/MembershipController.java
+++ src/main/java/com/example/membership/controller/MembershipController.java
@@ -1,7 +1,11 @@
 package com.example.membership.controller;
 
+import com.example.membership.common.DefaultRestController;
 import com.example.membership.dto.MembershipRequest;
 import com.example.membership.dto.MembershipResponse;
+import com.example.membership.entity.Membership;
+import com.example.membership.service.MembershipService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,12 +16,17 @@ import static com.example.membership.common.MembershipConstants.USER_ID_HEADER;
 
 @RestController
 @RequestMapping("/api/v1")
-public class MembershipController {
+@RequiredArgsConstructor
+public class MembershipController extends DefaultRestController {
+
+    private final MembershipService membershipService;
 
     @PostMapping("/membership")
     public ResponseEntity<MembershipResponse> addMembership(
             @RequestHeader(USER_ID_HEADER) final String userId,
             @RequestBody @Valid final MembershipRequest membershipRequest) {
+
+         membershipService.addMembership(userId, membershipRequest.getMembershipType(), membershipRequest.getPoint());
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
