77beea1a5da6d9727086a34c02511a5ca39b0f22
pebblepark <subling02@gmail.com>
2021-09-23 20:21:49 +0900

    controller 멤버십목록조회 구현

--- src/main/java/com/example/membership/controller/MembershipController.java
+++ src/main/java/com/example/membership/controller/MembershipController.java
@@ -1,6 +1,7 @@
 package com.example.membership.controller;
 
 import com.example.membership.common.DefaultRestController;
+import com.example.membership.dto.MembershipDetailResponse;
 import com.example.membership.dto.MembershipRequest;
 import com.example.membership.dto.MembershipAddResponse;
 import com.example.membership.service.MembershipService;
@@ -11,16 +12,17 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+import java.util.List;
+
 import static com.example.membership.common.MembershipConstants.USER_ID_HEADER;
 
 @RestController
-@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class MembershipController extends DefaultRestController {
 
     private final MembershipService membershipService;
 
-    @PostMapping("/membership")
+    @PostMapping("/api/v1/membership")
     public ResponseEntity<MembershipAddResponse> addMembership(
             @RequestHeader(USER_ID_HEADER) final String userId,
             @RequestBody @Valid final MembershipRequest membershipRequest) {
@@ -28,5 +30,10 @@ public class MembershipController extends DefaultRestController {
          final MembershipAddResponse membershipAddResponse = membershipService.addMembership(userId, membershipRequest.getMembershipType(), membershipRequest.getPoint());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(membershipAddResponse);
+    }
+
+    @GetMapping("/api/v1/membership/list")
+    public ResponseEntity<List<MembershipDetailResponse>> getMembershipList(@RequestHeader(USER_ID_HEADER) final String userId) {
+        return ResponseEntity.ok(membershipService.getMembershipList(userId));
     }
 }
