1c44169090e5b6a02f57809bafae813c8e2365ec
pebblepark <subling02@gmail.com>
2021-09-23 19:59:49 +0900

    사용자아이디로 멤버십조회 구현

--- src/main/java/com/example/membership/entity/MembershipRepository.java
+++ src/main/java/com/example/membership/entity/MembershipRepository.java
@@ -2,6 +2,9 @@ package com.example.membership.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
-    Membership findByUserIdAndMembershipType(String userId, MembershipType naver);
+    Membership findByUserIdAndMembershipType(String userId, MembershipType membershipType);
+    List<Membership> findAllByUserId(String userId);
 }
