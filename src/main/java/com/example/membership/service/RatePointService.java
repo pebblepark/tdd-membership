7310c38020e78946abba2806e4be9e54328a1dab
pebblepark <subling02@gmail.com>
2021-09-25 21:18:42 +0900

    멤버십 포인트 1% 적립

---
+++ src/main/java/com/example/membership/service/RatePointService.java
@@ -0,0 +1,13 @@
+package com.example.membership.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RatePointService {
+
+    private static final int RATE_POINT = 1;
+
+    public int calculateAmount(int price) {
+        return price * RATE_POINT / 100;
+    }
+}
